63e9151f516783c2a65d99177000e56b15a7e37f
Fentanylreactor <hasagi77@tutamail.com>
2026-08-21 06:34:01 +0200

    Refactor Main class to use init method for loading

--- src/example/Main.java
+++ src/example/Main.java
@@ -1,15 +1,21 @@
 package loadoutsizeincreaser;
 
+import arc.Events;
 import mindustry.Vars;
+import mindustry.game.EventType.ClientLoadEvent;
 import mindustry.game.Schematics;
 import mindustry.mod.Mod;
-import mindustry.world.*;
+import mindustry.world.*; 
 
 public class Main extends Mod {
 
-    public Main() {
-        Vars.schematics = new CustomSchematics();
-        Vars.schematics.load();
+    @Override
+    public void init() {
+        Events.on(ClientLoadEvent.class, event -> {
+            CustomSchematics custom = new CustomSchematics();
+            custom.load();
+            Vars.schematics = custom;
+        });
     }
 
     public static class CustomSchematics extends Schematics {
